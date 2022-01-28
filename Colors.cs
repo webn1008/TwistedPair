835c71b9b00ceedc7b6bf068353ac9115a4bda30
Tony Melvin <webn1008@gmail.com>
2022-01-27 23:08:48 -0500

    added JSON file

--- Colors.cs
+++ Colors.cs
@@ -7,5 +7,11 @@
             private string firstColor;
             private string secondColor;
         }
+
+        public class jsonColors
+        {
+            public IList<string> firstColor { get; set; }
+            public IList<string> secondColor { get; set; }
+        }
     }
 }
