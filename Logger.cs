5a116950e0a1fa010035acb12b657c33fea578b2
Tony Melvin <webn1008@gmail.com>
2022-02-19 01:03:07 -0500

    added logging

---
+++ Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace TwistedPair
+{
+    class Logger : LogBase
+    {
+        private String CurrentDirectory
+        {
+            get;
+            set;
+        }
+
+        private String FileName
+        {
+            get;
+            set;
+        }
+
+        private String FilePath
+        {
+            get;
+            set;
+        }
+
+        public Logger()
+        {
+            this.CurrentDirectory = Directory.GetCurrentDirectory();
+            this.FileName = "Log.txt";
+            this.FilePath = this.CurrentDirectory + "/" + this.FileName;
+
+        }
+
+        public override void Log(string Messsage)
+        {
+
+            //System.Console.WriteLine("Logged : {0}", Messsage);
+
+            using (System.IO.StreamWriter w = System.IO.File.AppendText(this.FilePath))
+            {
+                w.Write("\r\nLog Entry : ");
+                w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                w.WriteLine("  :{0}", Messsage);
+                w.WriteLine("-----------------------------------------------");
+            }
+        }
+    }
+}
